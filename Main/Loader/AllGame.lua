fbd82aa403ff981f7e67bfe9af525cd3880ed96f
Thunder Z Hub <143765336+ThundarZ@users.noreply.github.com>
2023-09-01 17:10:14 +0700

    Update and rename Bloxfruit.lua to AllGame.lua

---
+++ Main/Loader/AllGame.lua
@@ -0,0 +1,9 @@
+repeat wait() until game:IsLoaded()
+local PlaceId = game.PlaceId
+if PlaceId == 2753915549 or PlaceId == 4442272183 or PlaceId == 7449423635 then
+    if not game:GetService("UserInputService").TouchEnabled and not game:GetService("UserInputService").KeyboardEnabled == false then
+        loadstring(game:HttpGet('https://raw.githubusercontent.com/ThundarZ/Welcome/main/Main/BloxFruit/Main/PC.lua'))()
+    else
+        loadstring(game:HttpGet('https://raw.githubusercontent.com/ThundarZ/Welcome/main/Main/BloxFruit/Main/Mobile.lua'))()
+    end
+end
