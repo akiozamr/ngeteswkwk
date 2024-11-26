d52fab43a7d9bc2d039a8484c7dea8bdce91a1c6
Thunder Z Hub <143765336+ThundarZ@users.noreply.github.com>
2024-11-26 08:27:34 +0700

    Update AllGame.lua

--- Main/Loader/AllGame.lua
+++ Main/Loader/AllGame.lua
@@ -4,4 +4,6 @@ if PlaceId == 2753915549 or PlaceId == 4442272183 or PlaceId == 7449423635 then
     loadstring(game:HttpGet('https://raw.githubusercontent.com/ThundarZ/Welcome/main/Main/BloxFruit/Main/AllDevice.lua'))()
 elseif PlaceId == 6918802270 or PlaceId == 14979402479 then
     loadstring(game:HttpGet('https://raw.githubusercontent.com/ThundarZ/Welcome/main/Main/HazePiece/AllDevices.lua'))()
+elseif PlaceId == 16732694052 then
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/ThundarZ/Welcome/refs/heads/main/Main/Fisch/Main.lua))()
 end
