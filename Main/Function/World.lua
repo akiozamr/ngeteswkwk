54e7f065d108008e6a35e082a27fad7bf16dc8ec
Thunder Z Hub <143765336+ThundarZ@users.noreply.github.com>
2025-03-30 11:09:37 +0700

    Update World.lua

--- Main/Function/World.lua
+++ Main/Function/World.lua
@@ -1,10 +1,10 @@
 return {
-  ["Leveling City"] = {"Soondoo", "Gonshee", "Daek", "LongIn", "Anders", "Largalgan"}, order = 1 },
-  ["Grass Village"] = {"Snake Man", "Blossom", "Crow"}, order = 2 },
-  ["Brum Island"] = {"Shark Man", "Eminel", "Light Admiral"}, order = 3 },
-  ["Faceheal Town"] = {"Luryu", "Fyakuya", "Genji"}, order = 4 },
-  ["Lucky Kingdom"] = {"Sortudo", "Michille", "Wind"}, order = 5 },
-  ["Nippon City"] = {"Heaven", "Zere", "Ika"}, order = 6 },
-  ["Mori Town"] = {"Diablo", "Gosuke", "Golyne"}, order = 7 }
+  ["Leveling City"] = {"Soondoo", "Gonshee", "Daek", "LongIn", "Anders", "Largalgan"}, order = 1,
+  ["Grass Village"] = {"Snake Man", "Blossom", "Crow"}, order = 2,
+  ["Brum Island"] = {"Shark Man", "Eminel", "Light Admiral"}, order = 3,
+  ["Faceheal Town"] = {"Luryu", "Fyakuya", "Genji"}, order = 4,
+  ["Lucky Kingdom"] = {"Sortudo", "Michille", "Wind"}, order = 5,
+  ["Nippon City"] = {"Heaven", "Zere", "Ika"}, order = 6,
+  ["Mori Town"] = {"Diablo", "Gosuke", "Golyne"}, order = 7
     
 }
