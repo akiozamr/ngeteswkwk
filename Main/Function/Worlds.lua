645b8f8b369ed301f26ad3359152fa37b2c47388
Thunder Z Hub <143765336+ThundarZ@users.noreply.github.com>
2025-03-30 20:17:32 +0700

    Update Worlds.lua

--- Main/Function/Worlds.lua
+++ Main/Function/Worlds.lua
@@ -1,6 +1,6 @@
 return {
   ["Leveling City"] = { "Soondoo", "Gonshee", "Daek", "LongIn", "Anders", "Largalgan", order = 1 },
-  ["Grass Village"] = { "Snake Man", "Blossom", "Crow", order = 2 },
+  ["Grass Village"] = { "Snake Man", "Blossom", "Black Crow", order = 2 },
   ["Brum Island"] = { "Shark Man", "Eminel", "Light Admiral", order = 3 },
   ["Faceheal Town"] = { "Luryu", "Fyakuya", "Genji", order = 4 },
   ["Lucky Kingdom"] = { "Sortudo", "Michille", "Wind", order = 5 },
