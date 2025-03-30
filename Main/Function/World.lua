18b76de61a9c5ad42c6ef8bbad2be2405e38a32c
Thunder Z Hub <143765336+ThundarZ@users.noreply.github.com>
2025-03-30 10:58:17 +0700

    Create World.lua

---
+++ Main/Function/World.lua
@@ -0,0 +1,10 @@
+return {
+  ["Leveling City"] = {"Soondoo", "Gonshee", "Daek", "LongIn", "Anders", "Largalgan"},
+  ["Grass Village"] = {"Snake Man", "Blossom", "Crow"},
+  ["Brum Island"] = {"Shark Man", "Eminel", "Light Admiral"},
+  ["Faceheal Town"] = {"Luryu", "Fyakuya", "Genji"},
+  ["Lucky Kingdom"] = {"Sortudo", "Michille", "Wind"},
+  ["Nippon City"] = {"Heaven", "Zere", "Ika"},
+  ["Mori Town"] = {"Diablo", "Gosuke", "Golyne"}
+    
+}
