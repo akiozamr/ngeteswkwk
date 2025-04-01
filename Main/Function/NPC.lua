97279ce3ec085eaeb647595f97c5344579d0155c
Thunder Z Hub <143765336+ThundarZ@users.noreply.github.com>
2025-04-01 11:29:07 +0700

    Update NPC.lua

--- Main/Function/NPC.lua
+++ Main/Function/NPC.lua
@@ -1,11 +1,17 @@
-return {
-    ["Anders"] = CFrame.new(-164, 124, -297),
-    ["Largalgan"] = CFrame.new(-164, 124, -297),
-    ["SnakeMan"] = CFrame.new(-3386, 29, 2544),
-    ["Blossom"] = CFrame.new(-3429, 29, 2699),
-    ["Black Crow"] = CFrame.new(-3257, 30, 2867),
-    ["Shark Man"] = CFrame.new(-2851, 49, -2011),
-    ["Emel"] = CFrame.new(-3152, 58, -2349),
-    ["Light Admiral"] = CFrame.new(-3184, 56, -2656),
-    ["Luryu"] = CFrame.new(2641, 42, -2645),
-}
+	return {
+		["Anders"] = CFrame.new(-164, 124, -297),
+		["Largalgan"] = CFrame.new(-164, 124, -297),
+		["Snake Man"] = CFrame.new(-3386, 29, 2544),
+		["Blossom"] = CFrame.new(-3429, 29, 2699),
+		["Black Crow"] = CFrame.new(-3257, 30, 2867),
+		["Shark Man"] = CFrame.new(-2851, 49, -2011),
+		["Emel"] = CFrame.new(-3152, 58, -2349),
+		["Light Admiral"] = CFrame.new(-3184, 56, -2656),
+		["Luryu"] = CFrame.new(2641, 42, -2645),
+		["Fyakuya"] = CFrame.new(2964, 68, -3050),
+		["Genji"] = CFrame.new(3059, 45, -3327),
+		["Sortudo"] = CFrame.new(231, 37, 4488),
+		["Michille"] = CFrame.new(303, 37, 4763),
+		["Wind"] = CFrame.new(160, 39, 5121)
+	}
+	
