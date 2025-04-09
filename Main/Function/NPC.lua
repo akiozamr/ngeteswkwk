44802f10bab582bd1cc16b0ee536ddaf336a214e
Thunder Z Hub <143765336+ThundarZ@users.noreply.github.com>
2025-04-09 07:01:55 +0700

    Update NPC.lua

--- Main/Function/NPC.lua
+++ Main/Function/NPC.lua
@@ -5,7 +5,7 @@
 		["Blossom"] = CFrame.new(-3429, 29, 2699),
 		["Black Crow"] = CFrame.new(-3257, 30, 2867),
 		["Shark Man"] = CFrame.new(-2851, 49, -2011),
-		["Emel"] = CFrame.new(-3152, 58, -2349),
+		["Eminel"] = CFrame.new(-3152, 58, -2349),
 		["Light Admiral"] = CFrame.new(-3184, 56, -2656),
 		["Luryu"] = CFrame.new(2641, 42, -2645),
 		["Fyakuya"] = CFrame.new(2964, 68, -3050),
