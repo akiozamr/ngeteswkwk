fdad9e39c944c007302120967cac6d389dbab3ab
Thunder Z Hub <143765336+ThundarZ@users.noreply.github.com>
2025-06-11 10:09:46 +0700

    Create MutationHandler.lua

---
+++ Main/GaG/Module/MutationHandler.lua
@@ -0,0 +1,119 @@
+local module = {}
+
+local mutations = {
+    ["Chilled"] = {
+        Name = "Chilled",
+        ValueMulti = 2,
+        Color = Color3.fromRGB(135, 206, 250)
+    },
+    ["Moonlit"] = {
+        Name = "Moonlit",
+        ValueMulti = 2,
+        Color = Color3.fromRGB(153, 141, 255)
+    },
+    ["Twisted"] = {
+        Name = "Twisted",
+        ValueMulti = 5,
+        Color = Color3.fromRGB(191, 191, 191)
+    },
+    ["Burnt"] = {
+        Name = "Burnt",
+        ValueMulti = 5,
+        Color = Color3.fromRGB(40, 40, 40)
+    },
+    ["Choc"] = {
+        Name = "Choc",
+        ValueMulti = 2,
+        Color = Color3.fromRGB(92, 64, 51)
+    },
+    ["Frozen"] = {
+        Name = "Frozen",
+        ValueMulti = 10,
+        Color = Color3.fromRGB(108, 184, 255)
+    },
+    ["Wet"] = {
+        Name = "Wet",
+        ValueMulti = 2,
+        Color = Color3.fromRGB(64, 164, 223)
+    },
+    ["Shocked"] = {
+        Name = "Shocked",
+        ValueMulti = 100,
+        Color = Color3.fromRGB(255, 255, 100)
+    },
+    ["Bloodlit"] = {
+        Name = "Bloodlit",
+        ValueMulti = 4,
+        Color = Color3.fromRGB(200, 0, 0)
+    },
+    ["Zombified"] = {
+        Name = "Zombified",
+        ValueMulti = 25,
+        Color = Color3.fromRGB(128, 199, 127)
+    },
+    ["Celestial"] = {
+        Name = "Celestial",
+        ValueMulti = 120,
+        Color = Color3.fromRGB(255, 0, 255)
+    },
+    ["Disco"] = {
+        Name = "Disco",
+        ValueMulti = 125,
+        Color = Color3.fromRGB(255, 105, 180)
+    },
+    ["Plasma"] = {
+        Name = "Plasma",
+        ValueMulti = 5,
+        Color = Color3.fromRGB(208, 43, 137)
+    },
+    ["Voidtouched"] = {
+        Name = "Voidtouched",
+        ValueMulti = 135,
+        Color = Color3.fromRGB(225, 0, 255)
+    },
+    ["HoneyGlazed"] = {
+        Name = "HoneyGlazed",
+        ValueMulti = 5,
+        Color = Color3.fromRGB(255, 204, 0)
+    },
+    ["Pollinated"] = {
+        Name = "Pollinated",
+        ValueMulti = 3,
+        Color = Color3.fromRGB(255, 170, 0)
+    },
+    ["Windstruck"] = {
+        Name = "Windstruck",
+        ValueMulti = 5,
+        Color = Color3.fromRGB(255, 170, 0)
+    },
+    ["Dawnbound"] = {
+        Name = "Dawnbound",
+        ValueMulti = 5,
+        Color = Color3.fromRGB(255, 170, 0)
+    },
+    ["Heavenly"] = {
+        Name = "Heavenly",
+        ValueMulti = 5,
+        Color = Color3.fromRGB(255, 170, 0)
+    },
+}
+
+function module.GetMutations()
+    local list = {}
+    for _, data in pairs(mutations) do
+        table.insert(list, data)
+    end
+    return list
+end
+
+function module.CalcValueMulti(plant)
+    local valueMulti = 1
+    for _, data in pairs(mutations) do
+        if plant:GetAttribute(data.Name) then
+            valueMulti += (data.ValueMulti - 1)
+        end
+    end
+    return math.max(1, valueMulti)
+end
+
+return module
