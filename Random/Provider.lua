1971ec929dd7c60474e48523d7d591e7205ee73b
Thunder Z Hub <143765336+ThundarZ@users.noreply.github.com>
2025-07-12 10:25:14 +0700

    Create Provider.lua

---
+++ Random/Provider.lua
@@ -0,0 +1,6 @@
+return {
+    { name = "Rinku",       icon = "rbxassetid://123180234116541",  color = "#409fff", size = UDim2.new(0,18,0,18),locked = false },
+    { name = "LootLabs",    icon = "rbxassetid://113870826156188", color = "#c553ec", size = UDim2.new(0,22,0,22), locked = true },
+    { name = "Linkvertise", icon = "rbxassetid://132621208954136", color = "#FFA500", size = UDim2.new(0,26,0,26), locked = true },
+    { name = "Workink",     icon = "rbxassetid://77367436258901",  color = "#00ff00", size = UDim2.new(0,25,0,25), locked = true },
+}
